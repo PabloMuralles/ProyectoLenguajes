e68485b5141b402497b883305b059c59fcec3ec2
PabloMuralles <pablomuralles28@gmail.com>
2020-02-26 19:34:55 -0600

    paso 6 terminado

--- ArbolExpreciones.cs
+++ ArbolExpreciones.cs
@@ -11,15 +11,25 @@ namespace Proyecto_Lenguajes
 {
     class ArbolExpreciones
     {
-
+        // Variable que contiene la exprecion regular
         static string ExprecionRegularSets = string.Empty;
 
+        // Lista que contiene los simbolos terminales de la exprecion regular
         List<string> SimbolosTerminales = new List<string>();
+
+        //Lista que contiene los operadores de las expreciones regulares
         List<string> Operadores = new List<string>();
+
+        // Lista que contiene la exprecion regular tokenizada
         List<string> TokensExpresionSets = new List<string>();
+
+        // Pila de nodos para poder almacenar los arboles 
         Stack<Nodo> S = new Stack<Nodo>();
+
+        //Pila de string que almacena los tokens
         Stack<string> T = new Stack<string>();
-                           
+
+        /*Metodo para poder tokenizar la exprecion regular es decir separar por caracteres la exprecion regular*/                   
         public void ConvertirExprecionaTokens(string Cadena)
         {
             for (int i = 0; i < Cadena.Length; i++)
@@ -28,9 +38,10 @@ namespace Proyecto_Lenguajes
             }
         }
         
-        
+        // Metodo que sirve para ir insertando los datos al metodo de creacion de arbol para irlo creando
         public  void InsertarExprecionRegular()
         {
+             
             foreach (var item in TokensExpresionSets)
             {
                 Insertar_Arbol_Expreciones(item);
@@ -40,6 +51,7 @@ namespace Proyecto_Lenguajes
 
         }
 
+        //Metodo para poder inicializar los operador y simbolos terminales
         public void Crear_st_op()
         {
             Operadores.Add("*");
@@ -50,11 +62,11 @@ namespace Proyecto_Lenguajes
 
             SimbolosTerminales.Add("ID");
         }
-       
-        
 
-        
 
+
+        #region ARBOL DE EXPRECIONES
+        // Metodo para poder ir creando el arbol de expreciones
         public void Insertar_Arbol_Expreciones(string TokenExpresionRegular)
         {
             if (SimbolosTerminales.Contains(TokenExpresionRegular))
@@ -87,32 +99,56 @@ namespace Proyecto_Lenguajes
             }
             if (Operadores.Contains(TokenExpresionRegular))
             {
-                if (TokenExpresionRegular=="+" || TokenExpresionRegular == "." || TokenExpresionRegular == "*" || TokenExpresionRegular == "?")
+                if (TokenExpresionRegular == "+" || TokenExpresionRegular == "?" || TokenExpresionRegular == "*" )
                 {
                     Nodo TokenOp = new Nodo(TokenExpresionRegular);
-                    // duda si esto es menor porque no tiene sentido
-                    if (S.Count == 0)
+                   
+                    if (S.Count < 0)
                     {
                         throw new Exception("faltan operadandos");
                     }
                     TokenOp.Izquierdo = S.Pop();
-                    S.Push(TokenOp);
-                    if (true)
-                    {
-
-                    }
-                   
+                    S.Push(TokenOp);  
                 }
-                else
+                else if (T.Count != 0 && T.Peek()!="(" && (VerificarPrecedencia(TokenExpresionRegular,T.Peek()) == true))
+                {
+                    Nodo Temp = new Nodo(T.Pop());
+                    if (S.Count < 2)
+                    {
+                        throw new Exception("Faltan operandos");
+                    }
+                    // duda sobre este else preguntas
+                    else
+                    {
+                        Temp.Derecho = S.Pop();
+                        Temp.Izquierdo = S.Pop();
+                        S.Push(Temp);
+                    }
+                    
+
+                }               
+
+                if (TokenExpresionRegular != "*"|| TokenExpresionRegular != ".")
                 {
                     T.Push(TokenExpresionRegular);
-                        
                 }
 
             }
 
         }
-        
+#endregion
+
+        // Metodo para verficar el nivel de precedencia de un operador
+        // Devuelve un verdadero si el token es menor o igual en precedencial al operador ingresado
+        public bool VerificarPrecedencia(string TokenPrecedencia, string UltimoOperadorLista)
+        {
+            int IndexToken = Operadores.FindIndex(x => x.Equals(TokenPrecedencia));
+
+            int IndexUltimo = Operadores.FindIndex(x => x.Equals(TokenPrecedencia));
+
+            return IndexToken >= IndexUltimo;
+        }
+
 
     }
 }
