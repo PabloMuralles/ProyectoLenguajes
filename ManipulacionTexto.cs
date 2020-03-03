f6ee0770472aed053a7a5f66e66bd89cc1a53e3d
PabloMuralles <pablomuralles28@gmail.com>
2020-03-02 20:45:59 -0600

    primera parte sets exprecion regular

--- ManipulacionTexto.cs
+++ ManipulacionTexto.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
 
@@ -12,10 +12,15 @@ namespace Proyecto_Lenguajes
     {
         private StreamReader TextoEvaluar;
 
-    
- 
+        private int contador = 0;
 
- 
+        private string Contenido = string.Empty;
+
+         
+
+
+
+
 
         // contructor que resibe el texto del forms en forma de lista
         public ManipulacionTexto(StreamReader Texto)
@@ -27,12 +32,65 @@ namespace Proyecto_Lenguajes
 
         public void BuscarInicios()
         {
-            
-            string Contenido = TextoEvaluar.ReadLine();
-            int contador = 1;
+
+
+            Contenido = TextoEvaluar.ReadLine();
+            contador = 1;
             Contenido = QuitarCaracteres(Contenido);
 
-            while (Contenido != null && Contenido != "SETS")
+            while (Contenido != null)
+            {
+                if (Contenido == "" && Contenido != "SETS")
+                {
+                    Contenido = TextoEvaluar.ReadLine();
+                    Contenido = QuitarCaracteres(Contenido);
+                    contador = contador + 1;
+                }
+                else
+                {
+                    if (Contenido == "SETS")
+                    {
+                        ValidarSets();
+                        break;
+                    }
+                    else
+                    { 
+                        if (Contenido == "TOKENS")
+                        {
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error en la linea:   " + contador + "\n" + "Se esperaba un TOKEN");
+                            break;
+                        }
+
+                    }
+
+                }
+               
+               
+            }
+
+           
+
+             
+
+
+
+
+
+        }
+       
+
+        private Regex ExprecionSETS = new Regex(@"^([a-zA-ZñÑ\s])+=('([0-9a-zA-ZñÑ\s]{1})'|'([0-9a-zA-ZñÑ\s]{1})'\.\.'([0-9a-zA-ZñÑ\s]{1})')$");
+        public void ValidarSets()
+        {
+            
+            Contenido = TextoEvaluar.ReadLine();
+            contador = contador + 1;
+            Contenido = QuitarCaracteres(Contenido);
+            while (Contenido != null)
             {
                 if (Contenido == "")
                 {
@@ -42,36 +100,46 @@ namespace Proyecto_Lenguajes
                 }
                 else
                 {
-                    break;
-                }               
-               
+                    if (ExprecionSETS.IsMatch(Contenido))
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+
+                }  
+                
             }
-
-            if (Contenido == "TOKENS")
-            {
-
-            }
-            else
-            {
-                MessageBox.Show("Error en la linea:   "+ contador);
-            }
-
-             
-
-
-
-
 
         }
 
+
+
+
+
+
         public string QuitarCaracteres(string LineaEvaluar)
         {
+            string[] QuitrarEspacios = new string[LineaEvaluar.Length];
             // caracteres a quitar despues de quitar los caracteres no necesario
             char[] CaracteresDelimitadores = { '\t', '\r', ' ' };
 
             string NuevaLinea = LineaEvaluar.Trim(CaracteresDelimitadores);
+            QuitrarEspacios = NuevaLinea.Split(' ');
 
-            return NuevaLinea;
+            string LineaRetornar = string.Empty;
+
+            for (int i = 0; i < QuitrarEspacios.Length; i++)
+            {
+                if (QuitrarEspacios[i] != " ") 
+                {
+                    LineaRetornar += QuitrarEspacios[i];
+                }
+            }
+
+            return LineaRetornar;
          }
        
         
