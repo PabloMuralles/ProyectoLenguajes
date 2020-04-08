831fd4c8e92cd9291b91ea9d307510cf7b09c497
PabloMuralles <pablomuralles28@gmail.com>
2020-04-08 12:42:00 -0600

    la misma descripcion del commit anterior no se guardaron todos lo cambios

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.TablaFirstLast = new System.Windows.Forms.DataGridView();
+            this.TablaFollows = new System.Windows.Forms.DataGridView();
+            this.Simbolo1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Follow = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Simbolo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.First = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Last = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nulable = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TablaFollows = new System.Windows.Forms.DataGridView();
-            this.Simbolo1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Follow = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.TablaFirstLast)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TablaFollows)).BeginInit();
             this.SuspendLayout();
@@ -54,26 +54,6 @@
             this.TablaFirstLast.Size = new System.Drawing.Size(445, 208);
             this.TablaFirstLast.TabIndex = 0;
             this.TablaFirstLast.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // Simbolo
-            // 
-            this.Simbolo.HeaderText = "Simbolo";
-            this.Simbolo.Name = "Simbolo";
-            // 
-            // First
-            // 
-            this.First.HeaderText = "First";
-            this.First.Name = "First";
-            // 
-            // Last
-            // 
-            this.Last.HeaderText = "Last";
-            this.Last.Name = "Last";
-            // 
-            // Nulable
-            // 
-            this.Nulable.HeaderText = "Nulable";
-            this.Nulable.Name = "Nulable";
             // 
             // TablaFollows
             // 
@@ -99,6 +79,30 @@
             this.Follow.Name = "Follow";
             this.Follow.ReadOnly = true;
             // 
+            // Simbolo
+            // 
+            this.Simbolo.HeaderText = "Simbolo";
+            this.Simbolo.Name = "Simbolo";
+            this.Simbolo.ReadOnly = true;
+            // 
+            // First
+            // 
+            this.First.HeaderText = "First";
+            this.First.Name = "First";
+            this.First.ReadOnly = true;
+            // 
+            // Last
+            // 
+            this.Last.HeaderText = "Last";
+            this.Last.Name = "Last";
+            this.Last.ReadOnly = true;
+            // 
+            // Nulable
+            // 
+            this.Nulable.HeaderText = "Nulable";
+            this.Nulable.Name = "Nulable";
+            this.Nulable.ReadOnly = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -117,12 +121,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView TablaFirstLast;
+        private System.Windows.Forms.DataGridView TablaFollows;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Simbolo1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Follow;
         private System.Windows.Forms.DataGridViewTextBoxColumn Simbolo;
         private System.Windows.Forms.DataGridViewTextBoxColumn First;
         private System.Windows.Forms.DataGridViewTextBoxColumn Last;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nulable;
-        private System.Windows.Forms.DataGridView TablaFollows;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Simbolo1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Follow;
     }
 }
